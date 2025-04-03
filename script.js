c2b875cfbe12c750af171e65237c122e6d560c5f
Muhammad Zain <zain.ashiq613@gmail.com>
2025-04-03 17:16:07 +0500

    completed

--- script.js
+++
@@ -1,7 +0,0 @@
-let toggleBtn = document.getElementById("toggle-btn");
-let menu = document.getElementById("nav-links");
-
-toggleBtn.addEventListener("click", function () {
-  menu.classList.toggle("navbar-active");
-  console.log("clicked");
-});
